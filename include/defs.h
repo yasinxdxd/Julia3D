11fc354970f38194bd9fd9210d06e78ade7003a3
yasinxdxd <sesden19611@gmail.com>
2021-08-20 15:25:41 +0300

    some typedefs have been added.

--- include/defs.h
+++ include/defs.h
@@ -5,9 +5,15 @@
 #include <stddef.h>
 
 #define JULIA3D_CORE
+#define JULIA3D_COMPONENTS
 
 #define bool	int8_t
 #define true	1
 #define false	0
+
+typedef uint32_t ui32_t;
+typedef int32_t i32_t;
+typedef int8_t i8_t;
+typedef uint8_t ui8_t;
 
 #endif//_JULIA3D_DEFS_H_
