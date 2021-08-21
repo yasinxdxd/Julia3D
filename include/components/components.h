13687b7db0fa762d2f0a47bfac25f5261cb18389
yasinxdxd <sesden19611@gmail.com>
2021-08-21 17:50:10 +0300

    Transform component definitions are added and Texture is not a component anymore

--- include/components/components.h
+++ include/components/components.h
@@ -6,18 +6,22 @@
 struct _Transform;
 typedef struct _Transform* Transform;
 
+typedef float vec3[3];
+JULIA3D_COMPONENTS void j3d_transform_set_position(Transform transform, vec3 pos);
+JULIA3D_COMPONENTS void j3d_transform_set_rotation(Transform transform, vec3 rot);
+JULIA3D_COMPONENTS void j3d_transform_set_scale(Transform transform, vec3 scl);
+JULIA3D_COMPONENTS void j3d_transform_translate(Transform transform, vec3 pos);
+JULIA3D_COMPONENTS void j3d_transform_rotate(Transform transform, float angle, vec3 rot);
+JULIA3D_COMPONENTS void j3d_transform_rotate_x(Transform transform, float angle);
+JULIA3D_COMPONENTS void j3d_transform_rotate_y(Transform transform, float angle);
+JULIA3D_COMPONENTS void j3d_transform_rotate_z(Transform transform, float angle);
+JULIA3D_COMPONENTS void j3d_transform_scale(Transform transform, vec3 scl);
+
 struct _MeshFilter;
 typedef struct _MeshFilter* MeshFilter;
 
 struct _MeshRenderer;
 typedef struct _MeshRenderer* MeshRenderer;
-
-struct _Texture;
-typedef struct _Texture* Texture;
-
-JULIA3D_COMPONENTS bool j3d_texture_load(Texture texture, const char* file_path);
-JULIA3D_COMPONENTS bool j3d_texture_create(Texture texture);
-JULIA3D_COMPONENTS bool j3d_texture_destroy(Texture texture);
 
 struct _SpriteRenderer;
 typedef struct _SpriteRenderer* SpriteRenderer;
