9cd26b747f6383ddd32811dd36836c093b5a769a
yasinxdxd <sesden19611@gmail.com>
2021-08-23 18:12:16 +0300

    Added new c functions

--- julia_src/private/core.jl
+++ julia_src/private/core.jl
@@ -3,6 +3,7 @@ module j3dloader
     using Libdl
     libJulia3D = dlopen("../build/libJulia3D.so")
     
+    # window
     window_create = dlsym(libJulia3D, "j3d_window_create")
     window_initialize = dlsym(libJulia3D, "j3d_window_initialize")
     window_destroy = dlsym(libJulia3D, "j3d_window_destroy")
@@ -12,5 +13,21 @@ module j3dloader
     window_clear = dlsym(libJulia3D, "j3d_window_clear")
     window_display = dlsym(libJulia3D, "j3d_window_display")
 
+    # NON-COMPONENT CLASSES
+    # texture
+    texture_load = dlysm(libJulia3D, "j3d_texture_load")
+    texture_create = dlysm(libJulia3D, "j3d_texture_create")
+    texture_destroy = dlysm(libJulia3D, "j3d_texture_destroy")
 
+    # COMPONENTS
+    # transform
+    transform_set_position = dlysm(libJulia3D, "j3d_transform_set_position")
+    transform_set_rotation = dlysm(libJulia3D, "j3d_transform_set_rotation")
+    transform_set_scale = dlysm(libJulia3D, "j3d_transform_set_scale")
+    transform_translate = dlysm(libJulia3D, "j3d_transform_translate")
+    transform_rotate = dlysm(libJulia3D, "j3d_transform_rotate")
+    transform_rotate_x = dlysm(libJulia3D, "j3d_transform_rotate_x")
+    transform_rotate_y = dlysm(libJulia3D, "j3d_transform_rotate_y")
+    transform_rotate_z = dlysm(libJulia3D, "j3d_transform_rotate_z")
+    transform_scale = dlysm(libJulia3D, "j3d_transform_scale")
 end
