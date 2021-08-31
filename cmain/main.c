34e8313edb322e43a27e7b7793b5da50cd8e51d7
yasinxdxd <sesden19611@gmail.com>
2021-08-31 19:51:07 +0300

    Update main.c
    new features are tested.

--- cmain/main.c
+++ cmain/main.c
@@ -5,66 +5,56 @@
 #include <components/components.h>
 #include <stdlib.h>
 #include <vertex_array_buffer_object.h>
+#include <keyboard.h>
+
 
 int main()
 {
     struct _J3DWindow window;
-    j3d_window_create(&window, 600, 800, "Beberuhi");
+    j3d_window_create(&window, 1200, 200, "Beberuhi");
     j3d_window_initialize(&window);
 
-
-    struct _J3DShaderProgram shader;
-    j3d_shader_load_sources(&shader, "../shaders/default_2Dshape_renderer.vert", "../shaders/default_2Dshape_renderer.frag");
-    j3d_shader_compile_and_link(&shader);
-    j3d_shader_free_sources(&shader);
-
-
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
-
-    struct _VertexIndexArrayBufferObject viabo;
-    j3d_generate_viabo(&viabo);
-    j3d_bind_vibao(&viabo);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    j3d_unbind_vibao(&viabo);
-   
+    struct _SpriteRenderer sprite_renderer;
+    j3d_sprite_renderer_init(&sprite_renderer);
+    
+    struct _Texture texture;
+    j3d_texture_create(&texture, J3D_TEXTURE_2D, GL_REPEAT, GL_LINEAR);
+    j3d_texture_load(&texture, "../../../Downloads/Fruit.png");
+    /*
+    */
+    
+    j3d_sprite_renderer_set_texture(&sprite_renderer, &texture);
 
 
     while(!j3d_window_is_close(&window))
     {
         j3d_window_poll_event();
+        
 
 
-        /*CODE CODE*/
+        //CODE CODE
+
+        if(j3d_keyboard_is_key_pressed(&window, KEY_LEFT))
+            printf("LEFT key is pressed!\n");
+        if(j3d_keyboard_is_key_pressed(&window, KEY_RIGHT))
+            printf("RIGHT key is pressed!\n");
+        if(j3d_keyboard_is_key_pressed(&window, KEY_UP))
+            printf("UP key is pressed!\n");
+        if(j3d_keyboard_is_key_pressed(&window, KEY_DOWN))
+            printf("DOWN key is pressed!\n");
+
+        //CODE CODE
 
 
         j3d_window_clear(&window);
 
-        j3d_shader_use(&shader);
-        j3d_bind_vibao(&viabo);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        j3d_sprite_renderer_draw(&sprite_renderer);
 
         j3d_window_display(&window);
+        
     }
-
-    j3d_unbind_vibao(&viabo);
-    j3d_destroy_viabo(&viabo);
-    j3d_shader_destroy(&shader);
+    printf("dasd");
+    j3d_sprite_renderer_destroy(&sprite_renderer);
     j3d_window_destroy(&window);
 
 
